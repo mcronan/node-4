b47936bbfefa1f4fa0a5728e4a322e9b7bb9ba22
Ronan <ronanir600@gmail.com>
2015-07-23 20:00:30 -0600

    add route for noMagellen

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+var trip = ['seville','canary','capeverde','straitsofmagellan','guam','philipines']
+var counter = 0
+
+var next = function(){
+    var toNextCity = counter;
+    return trip[toNextCity];
+}
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index)
+app.get('/seville', indexController.seville)
+app.get('/CanaryIslands', indexController.canary)
+app.get('/CapeVerde', indexController.capeverde)
+app.get('/StraitsOfMagellan', indexController.straits)
+app.get('/guam', indexController.guam)
+app.get('/Phillipines', indexController.phil)
+
+
+app.post('/submit', function(req, res) {
+	counter ++
+	res.render(next())
+	console.log(counter)
+	if(counter > 5) {
+		res.redirect('/')
+	}
+
+})
+
+//*************** Bonus ******************//
+
+
+app.get('/:unknownDest', function(req, res) {
+	var userChoice = req.params.unknownDest
+	if(userChoice !== 'seville' || 'canary' || 'capeverde' || 'straitsofmagellan' || 'guam' || 'philipines' || 'noMagellen') {
+		res.redirect('/noMagellen')
+	}
+
+})
+
+
+
+
+var server = app.listen(3030, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
+
---
+++ controllers/index.js
@@ -0,0 +1,25 @@
+var indexController = {
+	index: function(req, res) {
+		res.render('index');
+	},
+	seville: function(req, res) {
+		res.render('seville');
+	},
+	canary: function(req, res){
+		res.render('canary');
+	},
+	capeverde: function(req, res){
+		res.render('capeverde');
+	},
+	straits: function(req, res){
+		res.render('straitsofmagellan');
+	},
+	guam: function(req, res){
+		res.render('guam');
+	},
+	phil: function(req, res){
+		res.render('philipines');
+	},
+};
+
+module.exports = indexController;
